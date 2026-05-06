d9565fe5f8fedb0343cba444497cac65eaeb5a09
Amanpreet Kaur <aman@gmail.com>
2026-05-06 15:56:58 +0530

    add

--- Reactproject/src/components/Checkout.jsx
+++ Reactproject/src/components/Checkout.jsx
@@ -163,7 +163,7 @@ const Checkout = () => {
   const userId = localStorage.getItem("userId");
 
   // ✅ 1. Sahi Backend URL set karein (wfyu wala jo aap use kar rahe hain)
-  const API_BASE_URL = "https://e-commerce-shopping-1-wfyu.onrender.com";
+  const API_BASE_URL = "https://e-commerce-shopping-cdqi.onrender.com";
 
   useEffect(() => {
     const fetchCart = async () => {
